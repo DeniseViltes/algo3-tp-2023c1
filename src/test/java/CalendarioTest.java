6c13c0fd5a5e17232db207c7d5064e636e3b2093
Denise <dviltes@fi.uba.ar>
2023-04-12 18:33:52 -0300

    Empecé unos test simples para probar los constructores "default"

---
+++ src/test/java/CalendarioTest.java
@@ -0,0 +1,33 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static org.junit.Assert.*;
+
+public class CalendarioTest {
+    @Test
+    public void crearEventoDefault() {
+        var calendario = new Calendario();
+
+        calendario.crearEvento("Evento nuevo",null);
+        var e = calendario.getEventos().get(0);
+        Assert.assertEquals(e.getTitulo(),"Evento nuevo");
+        assertNull(e.getDescripcion());
+        Assert.assertEquals(e.getFechaYHoraInicial(), LocalDateTime.of(LocalDate.now(), LocalTime.of(8,0)));
+    }
+
+    @Test
+    public void crearTareaDefault() {
+        var calendario = new Calendario();
+        calendario.crearTarea("Nueva tarea","hacer algo");
+
+        var t = calendario.getTareas().get(0);
+        Assert.assertEquals(t.getTitulo(),"Nueva tarea");
+        Assert.assertEquals(t.getDescripcion(),"hacer algo");
+        Assert.assertEquals(t.getVencimiento(), LocalDateTime.of(LocalDate.now(),LocalTime.of(19,0)));
+
+    }
+}
