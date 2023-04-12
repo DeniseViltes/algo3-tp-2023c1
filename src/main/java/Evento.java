12993477389654c3f8b9cf5c2ff6e59e6411e937
Denise <dviltes@fi.uba.ar>
2023-04-12 15:38:17 -0300

    Agregue setters en evento, y atributos de fecha y hora

--- src/main/java/Evento.java
+++ src/main/java/Evento.java
@@ -1,10 +1,21 @@
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Evento {
 
-    private final String titulo;
-    private final String descripcion;
-    public Evento(String t, String d) {
-        this.titulo = t;
-        this.descripcion = d;
+    private String titulo;
+    private String descripcion;
+    private LocalDateTime fechaYHoraInicial;
+    private LocalDateTime fechaYHoraFinal;
+    private boolean esDeDiaCompleto;
+    public Evento(String titulo, String descripcion) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+
+        //Dejo inicializado por ahora
+        this.fechaYHoraInicial = LocalDateTime.of(LocalDate.now(), LocalTime.of(8,0));
+        this.fechaYHoraFinal = LocalDateTime.of(LocalDate.now(),LocalTime.of(9,0));
     }
 
     public String getTitulo() {
@@ -15,5 +26,22 @@ public class Evento {
         return this.descripcion;
     }
 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setFechaYHoraInicial(LocalDateTime fechaYHoraInicial) {
+        this.fechaYHoraInicial = fechaYHoraInicial;
+    }
+
+    public void setFechaYHoraFinal(LocalDateTime fechaYHoraFinal) {
+        this.fechaYHoraFinal = fechaYHoraFinal;
+    }
+    public void setEsDeDiaCompleto(boolean esDeDiaCompleto) {
+        this.esDeDiaCompleto = esDeDiaCompleto;
+    }
 }
