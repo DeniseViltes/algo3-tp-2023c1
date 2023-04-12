473cb626569aa552751e3885c31f0aaa86e74d8c
Denise <dviltes@fi.uba.ar>
2023-04-12 18:31:47 -0300

    Agregue atributos,con sus getters y setters en tarea

--- src/main/java/Tarea.java
+++ src/main/java/Tarea.java
@@ -1,13 +1,26 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
 public class Tarea {
 
-    private final String titulo;
-    private final String descripcion;
+    private String titulo;
+    private String descripcion;
     private boolean completado;
+
+    private boolean esDeDiaCompleto;
+    private LocalDateTime vencimiento;
 
     public Tarea(String titulo, String descripcion) {
         this.titulo = titulo;
         this.descripcion = descripcion;
         this.completado = false;
+
+        //igual que en evento, dejo inicializado asi por ahora
+        // en google calendar se trunca media hora, no la hora completa
+        this.esDeDiaCompleto = false;
+        this.vencimiento = LocalDateTime.of(LocalDate.now(),LocalTime.now().truncatedTo(ChronoUnit.HOURS).plusMinutes(60));
     }
 
     public String getTitulo() {
@@ -20,5 +33,21 @@ public class Tarea {
 
     public void completarTarea() {
         this.completado = true;
+    }
+
+    public LocalDateTime getVencimiento() {
+        return vencimiento;
+    }
+
+    public void setVencimiento(LocalDateTime vencimiento) {
+        this.vencimiento = vencimiento;
+    }
+
+    public boolean isEsDeDiaCompleto() {
+        return esDeDiaCompleto;
+    }
+
+    public void setEsDeDiaCompleto(boolean esDeDiaCompleto) {
+        this.esDeDiaCompleto = esDeDiaCompleto;
     }
 }
