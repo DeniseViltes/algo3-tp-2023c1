628a7acdc37e047748261bf4fe82c23134305d1f
Denise <dviltes@fi.uba.ar>
2023-04-16 23:13:13 -0300

    Implementa repetir un evento, en base a RepeticionAnual

--- src/main/java/Calendario.java
+++ src/main/java/Calendario.java
@@ -13,36 +13,36 @@ public class Calendario {
     }
 
     // default crearEvento solo necesita la fecha del evento y la duracion default es de 1 hora
-    public Evento crearEvento(){
+    public Evento crearEvento() {
         var horaActualTruncada = LocalDateTime.now().truncatedTo(ChronoUnit.HOURS);
-        Evento evento = new Evento(horaActualTruncada);
+        Evento evento = new Evento(horaActualTruncada.plusHours(1));
         this.eventos.add(evento);
         return evento;
     }
 
     //Habria que ver como manejamos los errores
-    public Evento modificarTituloEvento(Evento evento, String titulo){
+    public Evento modificarTituloEvento(Evento evento, String titulo) {
         if (evento != null && titulo != null)
             evento.setTitulo(titulo);
-            return evento;
+        return evento;
     }
 
-    public void modificarDescripcionEvento(Evento evento, String descripcion){
+    public void modificarDescripcionEvento(Evento evento, String descripcion) {
         if (evento != null && descripcion != null)
             evento.setDescripcion(descripcion);
     }
 
-    public void modificarInicioEvento(Evento evento, LocalDateTime inicioEvento){
+    public void modificarInicioEvento(Evento evento, LocalDateTime inicioEvento) {
         if (evento != null && inicioEvento != null)
             evento.setInicio(inicioEvento);
     }
 
-    public void modificarFinalEvento(Evento evento, LocalDateTime finalEvento){
+    public void modificarFinalEvento(Evento evento, LocalDateTime finalEvento) {
         if (evento != null && finalEvento != null)
             evento.setFinal(finalEvento);
     }
 
-    public void modificarTipoEvento(Evento evento, boolean diaCompleto){
+    public void modificarTipoEvento(Evento evento, boolean diaCompleto) {
         if (evento != null)
             evento.setEsDeDiaCompleto(diaCompleto);
     }
@@ -50,79 +50,92 @@ public class Calendario {
     //Funciones a completar en un futuro cuando tengamos implementado repeticion y alarma
 
     //tendria que recibir una repeticion?
-    public void modificarRepeticionEvento(Evento evento, Repeticion repeticion){
+    public void modificarRepeticionEvento(Evento evento, Repeticion repeticion) {
+        evento.setRepeticion(repeticion);
     }
-    public void modificarAlarmaEvento(Evento evento, boolean alarma){
+
+    public void modificarAlarmaEvento(Evento evento, boolean alarma) {
     }
-    public void eliminarEvento(Evento evento){
+
+    public void eliminarEvento(Evento evento) {
         eventos.remove(evento);
     }
 
 
-
-
     // default crearTarea solo necesita el vencimiento
-    public void crearTarea(){
+    public void crearTarea() {
         var horaActualTruncada = LocalDateTime.now().truncatedTo(ChronoUnit.HOURS);
         Tarea tarea = new Tarea(horaActualTruncada);
         this.tareas.add(tarea);
     }
 
-    public void modificarTituloTarea(Tarea tarea, String titulo){
+    public void modificarTituloTarea(Tarea tarea, String titulo) {
         if (tarea != null && titulo != null)
             tarea.setTitulo(titulo);
     }
 
-    public void modificarDescripcionTarea(Tarea tarea, String descripcion){
+    public void modificarDescripcionTarea(Tarea tarea, String descripcion) {
         if (tarea != null && descripcion != null)
             tarea.setDescripcion(descripcion);
     }
-    public void modificarTipoTarea(Tarea tarea, boolean diaCompleto){
+
+    public void modificarTipoTarea(Tarea tarea, boolean diaCompleto) {
         if (tarea != null)
             tarea.setEsDeDiaCompleto(diaCompleto);
     }
 
-    public void modificarVencimientoTarea(Tarea tarea, LocalDateTime vencimiento){
+    public void modificarVencimientoTarea(Tarea tarea, LocalDateTime vencimiento) {
         if (tarea != null && vencimiento != null)
             tarea.setVencimiento(vencimiento);
     }
 
-    public void marcarTareaCompleta(Tarea tarea){
+    public void marcarTareaCompleta(Tarea tarea) {
         if (tarea != null)
             tarea.setEstado(true);
     }
-    public void marcarTareaIncompleta(Tarea tarea){
+
+    public void marcarTareaIncompleta(Tarea tarea) {
         if (tarea != null)
             tarea.setEstado(false);
     }
+
     // Hay que ver como implementamos la repeticion
-    public void modificarAlarmaTarea(Tarea tarea, boolean repeticion){
+    public void modificarAlarmaTarea(Tarea tarea, boolean repeticion) {
     }
-    public void eliminarTarea(Tarea tarea){
+
+    public void eliminarTarea(Tarea tarea) {
         tareas.remove(tarea);
     }
 
+    //el calendario almacena eventos, pero solo devuelve instancias de eventos, no el evento en si
 
-
-
-
-    public ArrayList<Evento> eventosDeLaFecha(LocalDate fecha){
-        var eventosdDelDia = new ArrayList<Evento>();
+    public ArrayList<InstanciaEvento> eventosDeLaFecha(LocalDate fecha) {
+        var eventosdDelDia = new ArrayList<InstanciaEvento>();
         var dia = fecha.atStartOfDay();
-        for (Evento i : eventos){
-            if (i.iniciaEntreLasFechas(dia, dia.plusDays(1)))
-                eventosdDelDia.add(i);
+        for (Evento i : eventos) {
+            if (i.iniciaEntreLosHorarios(dia, dia.plusDays(1)))
+
+                eventosdDelDia.add(new InstanciaEvento(i));
         }
         return eventosdDelDia;
     }
-    public ArrayList<Evento> eventosEntre (LocalDateTime inicio, LocalDateTime fin){
-        var listadoEventos = new ArrayList<Evento>();
-        for (Evento i : eventos){
-            while (i.iniciaEntreLasFechas(inicio,fin)){
-                listadoEventos.add(i);
+
+
+    public ArrayList<InstanciaEvento> eventosEntreFechas(LocalDateTime inicio, LocalDateTime fin) {
+        var listadoEventos = new ArrayList<InstanciaEvento>();
+        for (Evento i : eventos) {
+            if (i.iniciaEntreLosHorarios(inicio, fin))
+                listadoEventos.add( new InstanciaEvento(i));
+            var j = inicio;
+            while (i.tieneRepeticionEntreLosHorarios(j,fin)){
+                var instancia = new InstanciaEvento(i);
+                instancia.setFecha(i.proximaRepeticion(j));
+                listadoEventos.add(instancia);
+
+                j = i.proximaRepeticion(j);
             }
         }
         return listadoEventos;
-
     }
+
 }
--- src/main/java/Evento.java
+++ src/main/java/Evento.java
@@ -1,5 +1,3 @@
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
@@ -36,9 +34,7 @@ public class Evento {
     }
 
     //Funciones a implementar en un futuro cuando tengamos implementado repeticiones y alarmas
-    public void setRepeticion(boolean repeticion,Repeticion tipo){
-        if (!repeticion)
-            this.repeticion =null;
+    public void setRepeticion(Repeticion tipo){
         this.repeticion = tipo;
     }
     public void setAlarma(boolean alarma){ }
@@ -46,31 +42,30 @@ public class Evento {
 
 
 
-    // Estuve viendo como funciona en Google Calendar y se puede editar para que sea un evento de todo el dia o no.
-    // Y, cuando sacas que sea de todo el dia, vuelve a tener el horario que estaba configurado antes.
-    // Por eso, creo que convendria no truncar los horarios, sino, manejar eso en la etapa de la interfaz.
-    // O sea, dejar aca el horario completo y cuando se settea como evento de todo el dia, utilizar
-    // truncar para que solo se muestre el dia
-
-
-    public boolean iniciaEntreLasFechas(LocalDateTime inicio, LocalDateTime fin){
+    public boolean iniciaEntreLosHorarios(LocalDateTime inicio, LocalDateTime fin){
         //varifica si la fecha inicial del evento está entre las fechas dadas
-        if(!fechaYHoraInicial.isBefore(inicio) && !fechaYHoraInicial.isAfter(fin)){
-            return true;
-        }
-        LocalDateTime i = inicio;
-        //verifica si alguna de las repeticiones empieza entre las fechas
-        //LocalDateTime es inmutable
-        while (inicio.isBefore(fin)){
-            var Repeticion = repeticion.Repetir(i);
-            if(!fechaYHoraInicial.isBefore(Repeticion) && !fechaYHoraInicial.isAfter(Repeticion))
-                return true;
-            i.adjustInto(i.plusDays(1));//
-        }
-        return  false;
+        return !fechaYHoraInicial.isBefore(inicio) && !fechaYHoraInicial.isAfter(fin);
     }
 
-    public LocalDate getFechaInicial() {
-        return fechaYHoraInicial.toLocalDate();
+    public boolean tieneRepeticionEntreLosHorarios(LocalDateTime inicio, LocalDateTime fin){
+        if(repeticion == null)
+            return false;
+        var fechaRepeticion = repeticion.Repetir(inicio);
+        if(fechaRepeticion == null)
+            return  false;
+        return fechaRepeticion.isAfter(inicio) && fechaRepeticion.isBefore(fin);
+    }
+
+
+    public LocalDateTime getFechaInicial() {
+        return fechaYHoraInicial;
+    }
+
+    public boolean tieneRepeticion(){
+        return repeticion != null;
+    }
+
+    public  LocalDateTime proximaRepeticion (LocalDateTime inicio){
+        return repeticion.Repetir(inicio);
     }
 }
