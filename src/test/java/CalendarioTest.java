56d610782b31f32e4bfb7431f441ad48d78ebc3b
Denise <dviltes@fi.uba.ar>
2023-04-16 23:23:37 -0300

    Agrega tests basicos para cada tipo de repeticion anual

--- src/test/java/CalendarioTest.java
+++ src/test/java/CalendarioTest.java
@@ -4,27 +4,60 @@ import org.junit.Test;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-
-import static org.junit.Assert.*;
+import java.time.temporal.ChronoUnit;
 
 public class CalendarioTest {
+
+    //Eventos
     @Test
     public void crearEventoDefault() {
         var calendario = new Calendario();
+        var evento = calendario.crearEvento();
+        //despues agregar algunos getters para verificar que se creo bien
 
-        calendario.crearEvento(null);
-        var e = calendario.eventosDeLaFecha(LocalDate.now()).get(0);
 
-        Assert.assertEquals(e.getTitulo(),"My Event");
-        assertNull(e.getDescripcion());
-        Assert.assertEquals(e.getFechaInicial(), LocalDate.now());
+
+    }
+
+
+
+    @Test
+    public void eventoConUnaRepeticionAnual() {//tarda mucho este test
+        var calendario = new Calendario();
+        var evento = calendario.crearEvento();
+        var repeticionAnual = new RepeticionAnual();
+        repeticionAnual.setCantidadRepeticiones(1);
+
+        calendario.modificarRepeticionEvento(evento,repeticionAnual);
+        LocalDateTime fechaActual = LocalDate.now().atStartOfDay();
+        var listadoDeEventos = calendario.eventosEntreFechas(fechaActual,fechaActual.plusYears(1).plusDays(2));
+
+        Assert.assertEquals(2,listadoDeEventos.size());
+    }
+    @Test
+    public void eventoConRepeticionAnualConVencimiento(){
+        var calendario = new Calendario();
+        var evento = calendario.crearEvento();
+        var repeticionAnual = new RepeticionAnual();
+        var horaActualTruncada = LocalDateTime.now().truncatedTo(ChronoUnit.HOURS);
+        repeticionAnual.setVencimiento(LocalDateTime.of(LocalDate.now().plusYears(1), LocalTime.from(horaActualTruncada)));
+        calendario.modificarRepeticionEvento(evento,repeticionAnual);
+        var listadoDeEventos = calendario.eventosEntreFechas(horaActualTruncada,horaActualTruncada.plusYears(2));
+        Assert.assertEquals(2,listadoDeEventos.size());
     }
 
     @Test
-    public void crearTareaDefault() {
+    public void eventoConRepeticionAnualInfinita() {
         var calendario = new Calendario();
-        calendario.crearTarea("Nueva tarea","hacer algo");
+        var evento = calendario.crearEvento();
+        var repeticionAnual = new RepeticionAnual();
+        repeticionAnual.setRepeticionInfinita();
 
+        calendario.modificarRepeticionEvento(evento,repeticionAnual);
+        var horaActualTruncada = LocalDateTime.now().truncatedTo(ChronoUnit.HOURS);
+        var listadoDeEventos = calendario.eventosEntreFechas(horaActualTruncada,horaActualTruncada.plusYears(1).plusHours(1));
+        //arreglar esto para que no queden numeros random (sumo una hora por como se crea el evento default)
 
+        Assert.assertEquals(2,listadoDeEventos.size());
     }
 }
