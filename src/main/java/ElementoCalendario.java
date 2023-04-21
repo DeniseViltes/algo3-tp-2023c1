c82196b720691527ef7b50e07442e58b1b906e32
Denise <dviltes@fi.uba.ar>
2023-04-21 01:57:29 -0300

    Agrega getters para usar en las pruebas

--- src/main/java/ElementoCalendario.java
+++ src/main/java/ElementoCalendario.java
@@ -3,16 +3,16 @@ import java.time.LocalDateTime;
 
 //Pruebo poner una interfaz asi  no tenemos que duplicar todo para tarea
 public interface ElementoCalendario {
-
     void setTitulo(String titulo);
 
     void setDescripcion(String descripcion);
 
     void setFecha(LocalDateTime inicioEvento);
 
-    void setEsDeDiaCompleto(boolean diaCompleto);
+    String getTitulo ();
+    String getDescripcion ();
 
-    //creo que el evento viene con una alarma default siempre
+
     void agregarAlarmaAbsoluta(LocalDateTime horarioAlarma, Alarma.Efecto efecto);
     Alarma agregarAlarma(Duration intervalo, Alarma.Efecto efecto);
     void eliminarAlarma(Alarma alarma);
