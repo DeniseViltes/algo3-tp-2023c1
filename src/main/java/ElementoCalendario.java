f969651b6df4664235ca112290da5f82a20c282c
Denise <dviltes@fi.uba.ar>
2023-04-20 22:23:44 -0300

    Agregas metodos para modificar alarmas

--- src/main/java/ElementoCalendario.java
+++ src/main/java/ElementoCalendario.java
@@ -1,3 +1,4 @@
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 //Pruebo poner una interfaz asi  no tenemos que duplicar todo para tarea
@@ -12,9 +13,14 @@ public interface ElementoCalendario {
     void setEsDeDiaCompleto(boolean diaCompleto);
 
     //creo que el evento viene con una alarma default siempre
-    void agregarAlarma(LocalDateTime horarioAlarma, Alarma.Efecto efecto);
-
+    void agregarAlarmaAbsoluta(LocalDateTime horarioAlarma, Alarma.Efecto efecto);
+    Alarma agregarAlarma(Duration intervalo, Alarma.Efecto efecto);
     void eliminarAlarma(Alarma alarma);
-
     Alarma proximaAlarma(LocalDateTime dateTime);
+
+    void modificarIntervaloAlarma (Alarma alarma, Duration intervalo);
+
+    void modificarFechaAbsolutaAlarma(Alarma alarma, LocalDateTime fecha);
+
+    void modificarAlarmaEfecto (Alarma alarma, Alarma.Efecto efecto);
 }
