84d8c60ddc94fc31ce0a937d8af8804488111671
Denise <dviltes@fi.uba.ar>
2023-04-20 22:25:50 -0300

    Agrega las alarmas correspondientes a cada repeticion

--- src/main/java/InstanciaEvento.java
+++ src/main/java/InstanciaEvento.java
@@ -1,15 +1,25 @@
-import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.TreeMap;
 
 public class InstanciaEvento {
     private final Evento evento;
 
     private LocalDateTime fecha;
+    private TreeMap<LocalDateTime, Alarma> alarmasInstancia;
 
     public InstanciaEvento(Evento evento,LocalDateTime fecha) {
         this.evento = evento;
         this.fecha = fecha;
+        this.alarmasInstancia = new TreeMap<>();
+        cargarAlarmas();
     }
 
+    public void cargarAlarmas(){
+        var alarmasOriginales = evento.getAlarmas();
+        for (Alarma i : alarmasOriginales.values()){
+            i.setReferencia(fecha);
+            alarmasInstancia.put(i.getFechaYHora(),i);
+        }
+    }
 
 }
