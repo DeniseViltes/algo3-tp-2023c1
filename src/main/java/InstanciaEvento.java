209b21373bba1afac0736fa2fbdc8cbcb7861b4c
Denise <dviltes@fi.uba.ar>
2023-04-22 01:39:06 -0300

    Agrega que InstanciaEvento evento implemente ElementoCalendario

--- src/main/java/InstanciaEvento.java
+++ src/main/java/InstanciaEvento.java
@@ -2,7 +2,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.TreeMap;
 
-public class InstanciaEvento{
+public class InstanciaEvento implements ElementoCalendario{
 
     //tendria que implementar ElementoCalendario??
     private final Evento evento;
@@ -18,6 +18,7 @@ public class InstanciaEvento{
     }
 
     public void cargarAlarmas(){
+        alarmasInstancia.clear();
         var alarmasOriginales = evento.getAlarmas();
         for (Alarma i : alarmasOriginales.values()){
             if(!i.esDeFechaAbsoluta()) {
@@ -27,4 +28,66 @@ public class InstanciaEvento{
         }
     }
 
+
+    @Override
+    public void setTitulo(String titulo) {
+        evento.setTitulo(titulo);
+    }
+
+    @Override
+    public void setDescripcion(String descripcion) {
+        evento.setDescripcion(descripcion);
+    }
+
+    @Override
+    public void setFecha(LocalDateTime inicioEvento) {
+        evento.setFecha(inicioEvento);
+    }
+
+    @Override
+    public String getTitulo() {
+        return evento.getTitulo();
+    }
+
+    @Override
+    public String getDescripcion() {
+        return evento.getDescripcion();
+    }
+
+    @Override
+    public Alarma agregarAlarmaAbsoluta(LocalDateTime horarioAlarma, EfectoAlarma efecto) {
+        return  evento.agregarAlarmaAbsoluta(horarioAlarma,efecto);
+    }
+
+    @Override
+    public Alarma agregarAlarma(Duration intervalo, EfectoAlarma efecto) {
+        var alarma = evento.agregarAlarma(intervalo,efecto);
+       // cargarAlarmas();  ????
+        return alarma;
+    }
+
+    @Override
+    public void eliminarAlarma(Alarma alarma) {
+        evento.eliminarAlarma(alarma);
+    }
+
+    @Override
+    public Alarma proximaAlarma(LocalDateTime dateTime) {
+        return null;
+    }
+
+    @Override
+    public void modificarIntervaloAlarma(Alarma alarma, Duration intervalo) {
+
+    }
+
+    @Override
+    public void modificarFechaAbsolutaAlarma(Alarma alarma, LocalDateTime fecha) {
+
+    }
+
+    @Override
+    public void modificarAlarmaEfecto(Alarma alarma, EfectoAlarma efecto) {
+
+    }
 }
