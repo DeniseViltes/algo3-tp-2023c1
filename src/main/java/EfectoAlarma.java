06316fddb6909b049a1fb4b391ac5f25b8211c4c
Denise <dviltes@fi.uba.ar>
2023-06-19 04:04:53 -0300

    ANALIZAR SI SON CORRECTOS ESTOS CAMBIOS

--- src/main/java/EfectoAlarma.java
+++ src/main/java/EfectoAlarma.java
@@ -1,7 +1,24 @@
 import java.io.Serializable;
 
 public enum EfectoAlarma implements Serializable {
-    NOTIFICACION,
-    SONIDO,
-    MAIL,
+    NOTIFICACION("Notificacion"),
+    SONIDO("Sonido"),
+    MAIL("Mail");
+
+    private final String tipo;
+
+    EfectoAlarma(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return tipo;
+    }
+
+    //TODO ver si esta bien agregar esto
+    public static EfectoAlarma convertirStringAEfectoAlarma(String nombre){
+        return EfectoAlarma.valueOf(nombre.toUpperCase());
+    }
+
 }
