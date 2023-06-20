c46e9cb83f69e64e71dc40e43561d181c964a4bf
Denise <dviltes@fi.uba.ar>
2023-06-20 17:27:43 -0300

    Implementa el creador de alertas

--- src/main/java/CreadorDeAlerta.java
+++ src/main/java/CreadorDeAlerta.java
@@ -1,50 +1,52 @@
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.text.TextFlow;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.util.List;
 
 public class CreadorDeAlerta {
 
-    @FXML
-    private TextFlow descripcionAlerta;
-
-    @FXML
-    private Label tituloAlerta;
-
     public void mostrarAlerta (String titulo, List<String> descripcion) {
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("/alertas/alerta.fxml"));
-            HBox view = loader.load();
-            final Stage stage = new Stage();
-            Scene scene = new Scene(view);
-            stage.setScene(scene);
-            //TODO ver porque esto se supone que es null
-//            tituloAlerta.setText(titulo);
-//            tituloAlerta.setTextFill(Paint.valueOf("#e70e02ff"));
-//            tituloAlerta.setAlignment(Pos.CENTER);
-//            var listaDeTexto = new ArrayList<Text>();
-//            for(String i:descripcion){
-//                Text texto = new Text(i);
-//                texto.setTextAlignment(TextAlignment.CENTER);
-//                listaDeTexto.add(texto);
-//            }
-//            descripcionAlerta.getChildren().addAll(listaDeTexto);
-            stage.setResizable(false);
-            stage.initModality(Modality.APPLICATION_MODAL);
-            stage.showAndWait();
+
+        HBox alerta = new HBox(10);
+        alerta.setAlignment(Pos.CENTER);
+        alerta.setPadding(new Insets(25));
+
+        ImageView imagen = new ImageView(new Image("otros/attention.png"));
+        imagen.setFitHeight(150);
+        imagen.setFitWidth(125);
+
+        VBox contenedor = new VBox();
+        Label tituloAlerta = new Label(titulo);
+
+        tituloAlerta.setTextFill(Paint.valueOf("#e70e02ff"));
+        tituloAlerta.setAlignment(Pos.CENTER);
+
+        contenedor.getChildren().add(tituloAlerta);
+
+        for(String i:descripcion){
+            Label texto = new Label(i);
+            texto.setAlignment(Pos.CENTER);
+            contenedor.getChildren().add(texto);
         }
-        catch (IOException e){
-            throw new RuntimeException("error en mostrar alerta");
-        }
-    }
+
+        alerta.getChildren().add(imagen);
+        alerta.getChildren().add(contenedor);
+
+        final Stage stage = new Stage();
+        Scene scene = new Scene(alerta);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.show();    }
 
 }
 
