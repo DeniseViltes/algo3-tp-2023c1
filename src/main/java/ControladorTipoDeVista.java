92808b107560dbacd25aaba92ea6872211c63d96
Denise <dviltes@fi.uba.ar>
2023-06-19 23:51:50 -0300

    agrega interfaz

--- src/main/java/ControladorTipoDeVista.java
+++ src/main/java/ControladorTipoDeVista.java
@@ -1,7 +1,11 @@
-import java.time.LocalDateTime;
+import javafx.scene.Node;
 
 public interface ControladorTipoDeVista {
     void actualizarCalendario(Calendario calendario);
 
     void limpiarCalendario();
+    Node setear_texto_dia_completo(ElementoCalendario el, boolean tieneVencimiento);
+
+    Node setear_texto(ElementoCalendario el, boolean tieneVencimiento);
+
     }
